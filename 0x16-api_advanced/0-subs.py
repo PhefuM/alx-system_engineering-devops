76dec0d9b953dd007ded7c74ea64d072f95e2bc6
PhefuM <phefumadyo@gmail.com>
2024-01-16 06:40:52 -0800

    save

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and returns the number of subscribers"""
-
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
 
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API to retrieve subreddit details."""
-    headers = {
-        "User-Agent": "ubuntu:alx.APIadavncedProject:v1.0 \
-(by /u/don-wiseman-1903)"
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
     }
-    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    val = {
-        "limit": 50,
-        "count": 0
-    }
-    try:
-        r = requests.get(api_url, params=val, headers=headers,
-                         allow_redirects=False)
-        r.raise_for_status()
-        r_j = r.json()
-        if r_j:
-            return r_j['data']['subscribers']
-        return 0
-    except requests.exceptions.RequestException as e:
-        return 0
-    except ValueError as e:
-        return 0
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
